3e3267872359affbeb9e472253ec114c245ba9dc
murCATolog <murcatolog@gmail.com>
2025-03-18 12:00:16 +0200

    Finished the JS code and added the event handler.

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 //дана функція повертає обʼєкт проміс
-const loadData = async () => {
+loadData = async () => {
     try {
         // надсилаємо http запит на сервер за допомогою fetch
         // await чекаємо поки запит завершиться
         const response = await fetch('http://127.0.0.1:5500/quotes.json');
         // чекає і отримуємо дані і перетворюємо їх в json
         const data = await response.json();
-        const addData = (selector) => {
-            const element = document.querySelector('.' + selector);
-            return element.textContent = data[0][selector];
+        const addData = () => {
+            // функція рандомайзер
+            let getRandomQuote = () => Math.floor(Math.random() * data.length);
+            //додаємо елементи в DOM
+            let number = getRandomQuote();
+            let getQuote = document.querySelector('.quote');
+            let getAuthor = document.querySelector('.author');
+            getQuote.innerText = (data[number]['quote']);
+            getAuthor.innerText = (data[number]['author']);
         };
         
         if (data) {
-            addData('quote');
-            addData('author');
+            addData();
         };
     }
     // ловимо помилку
     catch (error) {
         console.error('Помилка отримання даних: ', error);
     }
-}
+};
 
-loadData();
-
-// // функція рандомайзер
-// const getRandomQuote = () => Math.floor(Math.random() * quotes.length);
-// console.log(quotes[getRandomQuote()]);
+//обробка кліку
+document.getElementById('button').addEventListener('click', () => loadData());
